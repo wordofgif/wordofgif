cebc4e72d1145c70a8b3d8c53a772f7fc7525680
Filip Noetzel <filip@j03.de>
2013-11-09 14:31:13 +0100

    add simple typeahead that already parses the srt

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,44 @@
+
 $(function() {
-  console.log('test')
+
   $('input').change(function() {
-    alert($(this).val())
+
+    var fs = require('fs');
+    var _ = require('lodash');
+
+    var file_name = $(this).val();
+
+    var file_content = fs.readFileSync(file_name);
+
+    var quotes = srt2data(file_content.toString());
+
+    $('.typeahead').typeahead({
+      name: 'quotes',
+      local: _.pluck(quotes, 'text')
+    });
+
   })
+
+  function srt2data(data, ms) {
+        var useMs = ms ? true : false;
+
+        data = data.replace(/\r/g, '');
+        var regex = /(\d+)\n(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})/g;
+        data = data.split(regex);
+        data.shift();
+
+        var items = [];
+        for (var i = 0; i < data.length; i += 4) {
+            items.push({
+                id: data[i].trim(),
+                startTime: useMs ? timeMs(data[i + 1].trim()) : data[i + 1].trim(),
+                endTime: useMs ? timeMs(data[i + 2].trim()) : data[i + 2].trim(),
+                text: data[i + 3].trim()
+            });
+        }
+
+        return items;
+    };
+
+
 })
