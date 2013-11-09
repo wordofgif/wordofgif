7def8bec9a9f2270d6140b1a97fd28afb1450953
Filip Noetzel <filip@j03.de>
2013-11-09 15:04:17 +0100

    use subtitles-parser via require()

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ $(function() {
 
     var fs = require('fs');
     var _ = require('lodash');
+    var srt2data = require('subtitles-parser').fromSrt;
 
     var file_name = $(this).val();
 
@@ -17,27 +18,6 @@ $(function() {
     });
 
   })
-
-  function srt2data(data, ms) {
-        var useMs = ms ? true : false;
-
-        data = data.replace(/\r/g, '');
-        var regex = /(\d+)\n(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})/g;
-        data = data.split(regex);
-        data.shift();
-
-        var items = [];
-        for (var i = 0; i < data.length; i += 4) {
-            items.push({
-                id: data[i].trim(),
-                startTime: useMs ? timeMs(data[i + 1].trim()) : data[i + 1].trim(),
-                endTime: useMs ? timeMs(data[i + 2].trim()) : data[i + 2].trim(),
-                text: data[i + 3].trim()
-            });
-        }
-
-        return items;
-    };
 
   $(window)
     .on('dragover', stop)
