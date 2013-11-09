5e8c89682ed3aa65884a04d5523250431a2b74b3
Andreas Koeberle <koeberle@gmail.com>
2013-11-09 14:09:07 +0100

    use drag/drop

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,36 @@
 $(function() {
-  console.log('test')
-  $('input').change(function() {
-    alert($(this).val())
-  })
+  $(window)
+    .on('dragover', stop)
+    .on('drop', stop);
+
+  var dropzone = $('#dropzone');
+
+  dropzone
+    .on('dragover', hover)
+    .on('dragleave', leave)
+    .on('dragend', leave)
+    .on('drop', ondrop);
+
+
+  
+  function ondrop(e) {
+    e.preventDefault();
+    var path = e.originalEvent.dataTransfer.files[0].path;
+    var video = document.getElementById('video');
+    console.log(path);
+  }
+
+  function hover() {
+    this.className = 'hover'
+  }
+
+  function leave() {
+    this.className = ''
+  }
+
+  function stop(e) {
+    e.preventDefault();
+  }
+
 })
+
