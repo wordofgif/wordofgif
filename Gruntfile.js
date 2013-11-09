a7a9916661a0daa6c10287f81cdbc655ed27af60
Andreas Koeberle <koeberle@gmail.com>
2013-11-09 11:52:57 +0100

    init project

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    nodewebkit: {
+      options: {
+        build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
+        mac: true, // We want to build it for mac
+        win: true, // We want to build it for win
+        linux32: false, // We don't need linux32
+        linux64: false // We don't need linux64
+      },
+      src: ['src/**/*'] // Your node-wekit app
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.registerTask('default', ['nodewebkit']);
+
+}
