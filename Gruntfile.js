f15ae3d1fd5f87ccbeb4d70fb1edf9a0d864d91b
Filip Noetzel <filip@j03.de>
2013-11-10 18:17:15 +0100

    don't build on win

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
         version: '0.8.0',
         build_dir: './webkitbuilds', // Where the build version of my node-webkit app is saved
         mac: true, // We want to build it for mac
-        win: true, // We want to build it for win
+        win: false, // We want to build it for win
         linux32: false, // We don't need linux32
         linux64: false // We don't need linux64
       },
